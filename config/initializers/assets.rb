3a32ae8aa773f8d43fdc8e87f5a4eadac6c38b79
ravi <ravichem06@outlook.com>
2015-11-23 03:05:39 +0530

    landing page created

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,16 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( coming-soon-style.css )
+Rails.application.config.assets.precompile += %w( font-awesome.css )
+Rails.application.config.assets.precompile += %w( social-icons.css )
+Rails.application.config.assets.precompile += %w( jquery-1.8.2.min.js )
+Rails.application.config.assets.precompile += %w( jquery.backstretch.min.js )
+Rails.application.config.assets.precompile += %w( jquery.tweet.js )
+Rails.application.config.assets.precompile += %w( jflickrfeed.js )
+Rails.application.config.assets.precompile += %w( coming-soon-scripts.js )
+
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
