bc62cbc0533cd39cd62edf8bf2050970d9fddf17
ravi <ravichem06@outlook.com>
2015-11-25 00:12:04 +0530

    added smtp settings to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => 'nameless-depths-1544.herokuapp.com' }
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+:address              => "smtp.gmail.com",
+:port                 => 587,
+:user_name            => ENV['GMAIL_SMTP_USER'],
+:password             => ENV['GMAIL_SMTP_PASSWORD'],
+:authentication       => 'plain',
+:enable_starttls_auto => true
+}
+
 end
