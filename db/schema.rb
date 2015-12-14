a2838e2aaeb4ea8c8f86bc7a75a2da8bb6a4a679
ravi <ravichem06@outlook.com>
2015-12-15 00:32:07 +0530

    created the page

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151123072720) do
+ActiveRecord::Schema.define(version: 20151213123206) do
 
   create_table "contacts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
