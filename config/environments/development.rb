3c1f40381e41d030565b1a16b33451a0dfedd6d4
Ravi <ravichem06@outlook.com>
2016-08-27 01:24:23 +0530

    updated 27 Aug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
 
-  Paperclip.options[:command_path] = 'C:\Program Files (x86)\GnuWin32\bin'
+  #Paperclip.options[:command_path] = 'C:\Program Files (x86)\GnuWin32\bin'
   
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
--- db/schema.rb
+++ db/schema.rb
@@ -45,9 +45,9 @@ ActiveRecord::Schema.define(version: 20160531064840) do
   create_table "communities", force: :cascade do |t|
     t.string   "name",                    limit: 255
     t.string   "title",                   limit: 255
-    t.text     "content",                 limit: 4294967295
-    t.datetime "created_at",                                 null: false
-    t.datetime "updated_at",                                 null: false
+    t.text     "content",                 limit: 65535
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
     t.string   "post_image_file_name",    limit: 255
     t.string   "post_image_content_type", limit: 255
     t.integer  "post_image_file_size",    limit: 4
